22681ed6ced228261fef4a3fbdd68d9014bccb94
Szymon Roter <szymon.roter@esatto.se>
2020-11-04 21:26:55 +0100

    commit2

--- tests23/tests23/Program.cs
+++ tests23/tests23/Program.cs
@@ -7,6 +7,9 @@ namespace tests23
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
